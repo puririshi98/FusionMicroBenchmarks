358ddca31183849575541c1f4b49b88db966524e
Rishi Puri <puririshi98@berkeley.edu>
2021-12-02 12:08:54 -0800

    Update torch_trt.py

--- Bert/torch_trt.py
+++ Bert/torch_trt.py
@@ -46,7 +46,7 @@ if __name__ == "__main__" :
    
     jit_model = torch.jit.script(model)
     sumtime = 0
-    for idx in range(10) :
+    for i in range(10) :
         if i>=5:
             since= time.time()
         out =  jit_model.forward(inputs, mask)
@@ -55,7 +55,7 @@ if __name__ == "__main__" :
     print("average runtime per iter for NVFUSER:", round(sumtime*1000.0/5.0,2), 'ms')
     trt_ts_module = torch_tensorrt.compile(jit_model,inputs=[inputs, mask],  enabled_precisions = {torch.float})
     sumtime = 0
-    for idx in range(10):
+    for i in range(10):
         if i>=5:
             since= time.time()
         out =  trt_ts_module(inputs, mask)
