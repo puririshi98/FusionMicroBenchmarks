b662e24d9a0668b1aca4ee8f2640ff92b2263fa6
Rishi Puri <puririshi98@berkeley.edu>
2021-12-06 14:53:28 -0800

    fixing sync

--- Bert/torch_trt.py
+++ Bert/torch_trt.py
@@ -50,9 +50,10 @@ if __name__ == "__main__" :
         if i>=5:
             since= time.time()
         out =  jit_model.forward(inputs, mask)
+        torch.cuda.synchronize()
         if i>=5:
             sumtime += time.time() - since
-        torch.cuda.synchronize()
+
     print("average runtime per iter for NVFUSER:", round(sumtime*1000.0/50.0,2), 'ms')
     trt_ts_module = torch_tensorrt.compile(jit_model,inputs=[inputs, mask],  enabled_precisions = {torch.float})
     sumtime = 0
@@ -60,7 +61,8 @@ if __name__ == "__main__" :
         if i>=5:
             since= time.time()
         out =  trt_ts_module(inputs, mask)
+        torch.cuda.synchronize()
         if i>=5:
             sumtime += time.time() - since
-        torch.cuda.synchronize()
+
     print("average runtime per iter for TORCH-TRT:", round(sumtime*1000.0/50.0,2), 'ms')
